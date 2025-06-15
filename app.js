22994f75cf97079320e2651a8f047220f7d1c99f
Slayn24 <charlescunanan052405@gmail.com>
2025-06-15 23:51:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+// Video preview for the projects //   
 const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 const video4 = document.getElementById('projectVideo4');
+const video5 = document.getElementById('projectVideo5');
+const video6 = document.getElementById('projectVideo6');
+const video7 = document.getElementById('projectVideo7');
+const video8 = document.getElementById('projectVideo8');
+const video9 = document.getElementById('projectVideo9');
+const video10 = document.getElementById('projectVideo10');
 
-// Sidebar elements //
-const sideBar = document.querySelector('.sidebar');
-const menu = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.close-icon')
 
 
 const hoverSign = document.querySelector('.hover-sign');
 
-const videoList =[video1, video2, video3, video4];
+const videoList =[video1, video2, video3, video4, video5, video6, video7, video8, video9, video10];
+// Video elements //                                                                                                                                                                                
 
+// Video Hover effect // 
 videoList.forEach (function(video){
     video.addEventListener("mouseover", function(){
         video.play()
@@ -22,16 +27,4 @@ videoList.forEach (function(video){
     video.pause();
     hoverSign.classList.remove("active")
 })
-})
-
-// Sidebar elements //
-menu.addEventListener("click", function(){
-    sideBar.classList.remove("close-sidebar")
-    sideBar.classList.add("open-sidebar")
-});
-
-closeIcon.addEventListener("click", function(){
-    sideBar.classList.remove("open-sidebar");
-    sideBar.classList.add("close-sidebar");
-    
 })
